af9298df5e032ae12503f0c60a86a3ba491c1b0f
Björn Rasmusson <B.Rasmusson@computer.org>
2017-04-30 21:03:11 +0200

    gherkin: (C) Handle multi language filenames on Windows.
    The Gherkin library assumes that the feature filenames are encoded in
    UTF-8. On Windows that most likely is not true. Even if Windows are set
    to use unicode, that usually is UTF-16 and not UTF-8. Therefore, on
    Windows, use a Windows API to convert from the encoding of the CLI
    arguments (feature filename) and the internal wide character
    representation.

--- gherkin/c/src/string_utilities.c
+++ gherkin/c/src/string_utilities.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+
 typedef struct StringUtf8Source {
     Utf8Source utf8_source;
     int position;
@@ -17,6 +21,8 @@ static void StringUtf8Source_delete(Utf8Source* utf8_source);
 
 static unsigned char StringUtf8Source_read(Utf8Source* utf8_source);
 
+#endif /* ifdef _WIN32 ... else ... */
+
 wchar_t* StringUtilities_copy_string(const wchar_t* string) {
     return StringUtilities_copy_string_part(string, wcslen(string));
 }
@@ -27,6 +33,8 @@ wchar_t* StringUtilities_copy_string_part(const wchar_t* string, const int lengt
     copy[length] = L'\0';
     return copy;
 }
+
+#ifndef _WIN32
 
 wchar_t* StringUtilities_copy_to_wide_string(const char* string) {
     int length = strlen(string);
@@ -52,28 +60,6 @@ wchar_t* StringUtilities_copy_to_wide_string(const char* string) {
     return copy;
 }
 
-size_t StringUtilities_code_point_length(const wchar_t* string) {
-    if (sizeof(wchar_t) > 2) {
-        return wcslen(string);
-    } else {
-        return StringUtilities_code_point_length_for_part(string, wcslen(string));
-    }
-}
-
-size_t StringUtilities_code_point_length_for_part(const wchar_t* string, const int length) {
-    int code_points = 0;
-    int i;
-    for (i = 0; i < length; ++i) {
-        ++code_points;
-        if (UnicodeUtilities_is_utf16_surrogate(string[i])) {
-            ++i;
-        }
-
-    }
-    return code_points;
-}
-
-
 Utf8Source* StringUtf8Source_new(const char* string) {
     StringUtf8Source* string_utf8_source = (StringUtf8Source*)malloc(sizeof(StringUtf8Source));
     string_utf8_source->utf8_source.read = &StringUtf8Source_read;
@@ -97,4 +83,58 @@ unsigned char StringUtf8Source_read(Utf8Source* utf8_source) {
         return string_utf8_source->string[string_utf8_source->position++];
     }
     return EOF;
+}
+
+#else /* _WIN32 defined */
+
+LPWSTR StringUtilities_copy_to_wide_string(const char* string) {
+    /* int MultiByteToWideChar(
+            _In_      UINT   CodePage,
+            _In_      DWORD  dwFlags,
+            _In_      LPCSTR lpMultiByteStr,
+            _In_      int    cbMultiByte,
+            _Out_opt_ LPWSTR lpWideCharStr,
+            _In_      int    cchWideChar
+       ); */
+    /* Let  MultiByteToWideChar calculate the required buffer size */
+    int buffer_size = MultiByteToWideChar(CP_THREAD_ACP, 0, string, -1, 0, 0);
+    if (buffer_size == 0 || buffer_size == 0xFFFD) { /* error, return empty string (0xFFFD -> UTF byte sequence invalid) */
+        LPWSTR copy = (wchar_t*)malloc(1 * sizeof(wchar_t));
+        copy[0] = L'\0';
+        return copy;
+    }
+    /* The + 1 should not be necessary since the calculated buffer size
+       includes the terminating null character, but MinGW compilers
+       produce an unstable executable when the + 1 is not there. */
+    LPWSTR copy = (wchar_t*)malloc((buffer_size + 1) * sizeof(wchar_t));
+    int result = MultiByteToWideChar(CP_THREAD_ACP, 0, string, -1, copy, buffer_size);
+    if (result == 0 || result != buffer_size) { /* error, return empty string */
+        copy[0] = L'\0';
+    } else {
+        copy[buffer_size] = L'\0';
+    }
+    return copy;
+}
+
+#endif /* ifndef _WIN32 ... else ... */
+
+size_t StringUtilities_code_point_length(const wchar_t* string) {
+    if (sizeof(wchar_t) > 2) {
+        return wcslen(string);
+    } else {
+        return StringUtilities_code_point_length_for_part(string, wcslen(string));
+    }
+}
+
+size_t StringUtilities_code_point_length_for_part(const wchar_t* string, const int length) {
+    int code_points = 0;
+    int i;
+    for (i = 0; i < length; ++i) {
+        ++code_points;
+        if (UnicodeUtilities_is_utf16_surrogate(string[i])) {
+            ++i;
+        }
+
+    }
+    return code_points;
 }
